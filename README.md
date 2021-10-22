1781d25d11e1f82999820561d0096c3e004a16dd
Luiz Carlos de Souza Leite <luiz.leite@agrotis.com>
2021-10-21 23:20:55 -0300

    feat(Product): implements CRUD

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # cayena-products
 This project is a result of a test made for apply to Cayena position
+
+## Installation
+
+Clone [this](https://github.com/luiz-leite-dev/cayena-products.git) repository:
+
+```bash
+git clone https://github.com/luiz-leite-dev/cayena-products.git
+```
+
+## Usage
+
+You need a mysql database called test at `//localhost/test` and a user `root` with password `masterkey`, then you can run the application with this command:
+```bash
+mvn spring-boot:run
+```
+
+You can test with Postman importing this collection `\src\main\resources\Cayena-Products.postman_collection.json`
---
+++ src/main/java/com/cayena/products/entities/Product.java
@@ -0,0 +1,97 @@
+package com.cayena.products.entities;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "quantity_in_stock")
+	private Long quantityInStock;
+
+	@Column(name = "unit_price")
+	private BigDecimal unitPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "supplier_id")
+	private Supplier supplier;
+
+	@Column(name = "date_of_creation")
+	private Date dateOfCreation;
+
+	@Column(name = "date_of_the_last_update")
+	private Date dateOfTheLastUpdate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getQuantityInStock() {
+		return quantityInStock;
+	}
+
+	public void setQuantityInStock(Long quantityInStock) {
+		this.quantityInStock = quantityInStock;
+	}
+
+	public BigDecimal getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(BigDecimal unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public Supplier getSupplier() {
+		return supplier;
+	}
+
+	public void setSupplier(Supplier supplier) {
+		this.supplier = supplier;
+	}
+
+	public Date getDateOfCreation() {
+		return dateOfCreation;
+	}
+
+	public void setDateOfCreation(Date dateOfCreation) {
+		this.dateOfCreation = dateOfCreation;
+	}
+
+	public Date getDateOfTheLastUpdate() {
+		return dateOfTheLastUpdate;
+	}
+
+	public void setDateOfTheLastUpdate(Date dateOfTheLastUpdate) {
+		this.dateOfTheLastUpdate = dateOfTheLastUpdate;
+	}
+}
